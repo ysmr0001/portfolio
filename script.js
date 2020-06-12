42feddad23b6af030e990664a7b3d01a602bf7c8
ysmr0001 <adds.3.luvmid@gmail.com>
2020-06-12 13:54:31 +0900

    changed css

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ document.getElementById("test").textContent = "今日のラッキーナンバー
 const kuji = ["大吉", "中吉", "小吉", "末吉", "まぁまぁ", "凶"];
 const kujin = Math.floor(Math.random() * kuji.length);
 document.getElementById("kuji").textContent = "凶の運勢は" + kuji[kujin];
+
+
+document.getElementById("price")
